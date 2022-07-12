7ab34a93ed58946c6d1928949b7889492602f819
aname <47141166+as06zx@users.noreply.github.com>
2022-07-13 00:30:23 +0900

    logger.lua

---
+++ logger.lua
@@ -0,0 +1,22 @@
+local function bpLoad()
+   local length = readInteger(RAX+0x10)
+   local data   = readString(RAX+0x20, length)
+   printf("load -> %s", data)
+end
+
+local function bpAsm()
+   local p = readPointer(RCX)
+   local length = readInteger(p-0x8)
+   local data   = readString(p, length)
+   printf("autoAssemble -> %s", data)
+end
+
+local function bpDecode()
+   local data = readString(RAX, 6000)
+   printf("decodeFunction -> %s", data)
+end
+
+debugProcess()
+debug_setBreakpoint("lua53-64.luaL_checkversion_+1906", bpLoad)
+debug_setBreakpoint("cheatengine-x86_64-SSE4-AVX2.exe+264DBA", bpAsm)
+debug_setBreakpoint("cheatengine-x86_64-SSE4-AVX2.exe+27F585", bpDecode)
