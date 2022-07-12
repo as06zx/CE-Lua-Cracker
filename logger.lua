dc580aec4821bfcb40f238889b3705909efa1ba1
aname <47141166+as06zx@users.noreply.github.com>
2022-07-13 01:19:34 +0900

    Added more func
    shellExecute , createProcess

--- logger.lua
+++ logger.lua
@@ -16,7 +16,22 @@ local function bpDecode()
    printf("decodeFunction -> %s", data)
 end
 
+local function bpShell()
+   local length = readInteger(RDX-0x8)
+   local data   = readString(RDX, length)
+   printf("shellExecute -> %s", data)
+end
+
+local function bpCreate()
+   local length = readInteger(RCX-0x8)
+   local data   = readString(RCX, length)
+   printf("createProcess -> %s", data)
+end
+
 debugProcess()
 debug_setBreakpoint("lua53-64.luaL_checkversion_+1906", bpLoad)
 debug_setBreakpoint("cheatengine-x86_64-SSE4-AVX2.exe+264DBA", bpAsm)
 debug_setBreakpoint("cheatengine-x86_64-SSE4-AVX2.exe+27F585", bpDecode)
+debug_setBreakpoint("cheatengine-x86_64-SSE4-AVX2.exe+272B56", bpShell)
+debug_setBreakpoint("cheatengine-x86_64-SSE4-AVX2.exe+2682CD", bpCreate)
+
